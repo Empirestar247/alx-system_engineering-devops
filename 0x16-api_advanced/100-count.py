cce3fa18731304cc0220b987a6d998c9deedfd40
Empirestar247 <ejimofor81@gmail.com>
2023-11-07 15:27:10 -0800

    API advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,50 +1,60 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
-
-
+"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
+def count_words(subreddit, word_list, after=None, counts={}):
+    """Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        after (str): The parameter for the next page of the API results.
+        counts (dict): The parameter of results matched thus far.
     """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    if not word_dict:
-        for word in word_list:
-            if word.lower() not in word_dict:
-                word_dict[word.lower()] = 0
+    # Set a custom User-Agent and disable following redirects
+    headers = {'User-Agent': 'RedditDataAnalyzer/1.0 (ALX Africa)'}
+    params = {'limit': 100}  # Limit the number of posts to 100 (maximum)
 
-    if after is None:
-        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word in wordict:
-            if word[1]:
-                print('{}: {}'.format(word[0], word[1]))
-        return None
+    if after:
+        params['after'] = after
 
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    header = {'user-agent': 'redquery'}
-    parameters = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=header, params=parameters,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
-    if response.status_code != 200:
-        return None
+    if response.status_code == 200:
+        data = response.json()
 
-    try:
-        hot = response.json()['data']['children']
-        aft = response.json()['data']['after']
-        for post in hot:
-            title = post['data']['title']
-            lower = [word.lower() for word in title.split(' ')]
+        # Extract and parse the titles of the posts
+        for post in data.get('data', {}).get('children', []):
+            title = post.get('data', {}).get('title', '').lower()
+            words = title.split()
 
-            for word in word_dict.keys():
-                word_dict[word] += lower.count(word)
+            # Count the occurrences of keywords in the title
+            for word in word_list:
+                if word.lower() in words:
+                    times = words.count(word.lower())
+                    if counts.get(word) is None:
+                        counts[word] = times
+                    else:
+                        counts[word] += times
 
-    except Exception:
-        return None
+        # Check if there are more pages (pagination) and continue the recursion
+        after = data.get('data', {}).get('after')
+        if after:
+            return count_words(subreddit, word_list, after, counts)
 
-    count_words(subreddit, word_list, aft, word_dict))
+        if len(counts) == 0:
+            return
+
+        # If no more pages, print the sorted results
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print("{}: {}".format(word, count))
+
+    elif response.status_code == 404:
+        return
+    else:
+        return
