d16efe162e9397d1d9f3af4cfc061cf859a1d8a5
Empirestar247 <ejimofor81@gmail.com>
2023-11-13 01:42:15 -0800

    debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,13 @@
-# to login with the holberton user and open a file without any error message.
-file { 'loginFile':
-    ensure  => present,
-    path    => '/etc/security/limits.conf',
-    content => '#File erased'
+#Enable the user holberton to login and open files without errors
+
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
+
+#Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
 }
